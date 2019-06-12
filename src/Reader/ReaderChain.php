64430a0bc8b2be5b807817b8e7a0f5ee5df972c8
Benjamin Morel <benjamin.morel@gmail.com>
2019-06-12 17:07:32 +0200

    Add ReaderChain implementation

---
+++ src/Reader/ReaderChain.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Schema\Reader;
+
+use Brick\Schema\SchemaReader;
+
+use DOMDocument;
+
+/**
+ * Chains several schema readers and returns the aggregate results.
+ */
+class ReaderChain implements SchemaReader
+{
+    /**
+     * @var SchemaReader[]
+     */
+    private $readers;
+
+    /**
+     * ReaderChain constructor.
+     *
+     * @param SchemaReader ...$readers
+     */
+    public function __construct(SchemaReader ...$readers)
+    {
+        $this->readers = $readers;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function read(DOMDocument $document, string $url) : array
+    {
+        if (! $this->readers) {
+            return [];
+        }
+
+        $items = [];
+
+        foreach ($this->readers as $reader) {
+            $items[] = $reader->read($document, $url);
+        }
+
+        return array_merge(...$items);
+    }
+}
